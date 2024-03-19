50190ac40a8296ef630df156de69902fa8d0b023
Daka <125908733+DakaRalho@users.noreply.github.com>
2024-03-19 17:13:57 -0300

    Create audio_worker.js

---
+++ audio_worker.js
@@ -0,0 +1,26 @@
+let context = null;
+
+self.onmessage = function(e) {
+    if (e.data === 'start') {
+        startAudio();
+    } else if (e.data === 'stop') {
+        stopAudio();
+    }
+};
+
+function startAudio() {
+    context = new AudioContext();
+    let oscillator = context.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 440; // Exemplo de frequência
+    oscillator.connect(context.destination);
+    oscillator.start(0);
+}
+
+function stopAudio() {
+    if (context) {
+        context.close().then(function() {
+            context = null;
+        });
+    }
+}
