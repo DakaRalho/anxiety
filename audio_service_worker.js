35981da2836b0f63cf727ff210ac7f2864787fae
Daka <125908733+DakaRalho@users.noreply.github.com>
2024-03-20 12:07:24 -0300

    Update audio_service_worker.js

--- audio_service_worker.js
+++ audio_service_worker.js
@@ -1,4 +1,4 @@
-let audio = new Audio('Kim Dracula - Make Me Famous (Official Audio)(M4A_128K).m4a'); // Substitua 'nome_do_arquivo.mp3' pelo nome do seu arquivo de áudio
+let audio = new Audio('Kim Dracula - Make Me Famous (Official Audio)(MP3_160K).mp3'); // Substitua 'nome_do_arquivo.mp3' pelo nome do seu arquivo de áudio
 
 self.addEventListener('message', function(e) {
     if (e.data === 'start') {
